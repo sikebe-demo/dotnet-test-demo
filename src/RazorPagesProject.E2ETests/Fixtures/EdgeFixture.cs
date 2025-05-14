f3d82ef2664edc8ca93148c81070fa6ed7a4671c
SIkebe <SIkebe@users.noreply.github.com>
2025-05-14 22:22:57 +0900

    feat: Add workaround for EdgeDriver compatibility issue

--- src/RazorPagesProject.E2ETests/Fixtures/EdgeFixture.cs
+++ src/RazorPagesProject.E2ETests/Fixtures/EdgeFixture.cs
@@ -18,6 +18,9 @@ public class EdgeFixture : BrowserFixture
 
         opts.AddArguments("-inprivate");
 
+        // Workaround for https://github.com/SeleniumHQ/selenium/issues/15340
+        opts.AddArgument("--edge-skip-compat-layer-relaunch");
+
         // Comment this out if you want to watch or interact with the browser (e.g. for debugging)
         if (!Debugger.IsAttached)
         {
